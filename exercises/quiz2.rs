69e5dd0e818bcca45ad0b4b3304327b95db750b5
akizhang <observelily@gmail.com>
2023-01-06 11:44:49 +0800

    Finish errors options

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,7 +18,6 @@
 // - The output element is going to be a Vector of strings.
 // No hints this time!
 
-// I AM NOT DONE
 
 pub enum Command {
     Uppercase,
@@ -29,12 +28,23 @@ pub enum Command {
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
-        for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = vec![];
+        // 用input.iter产生的是数组元素的不可变引用，不能修改string和times！
+        // 因为input是Vec<T>，into_iter获得的是元素T而不是引用
+        // https://stackoverflow.com/questions/34733811/what-is-the-difference-between-iter-and-into-iter
+        for (string, command) in input.into_iter() {
+            match command {
+                Command::Uppercase => {
+                    output.push(string.to_uppercase());
+                }
+                Command::Trim => {
+                    output.push(string.trim().to_string());
+                }
+                Command::Append(times) => {
+                    output.push(string + &"bar".repeat(times));
+                }
+            }
         }
         output
     }
@@ -42,8 +52,7 @@ mod my_module {
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we have to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
